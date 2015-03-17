7726dcb1012f3d7877b0e7ce0c8387f8ee8d922f
Serdar Akarca <serdar@yuix.org>
2015-03-17 15:30:00 +0200

    configs

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 module.exports = {
 
   // Stuart server parameters required for Twilio integration
-  "host"   : "http://FIXME",
+  "host"   : "http://localhost",
   "port"   : "8087",
 
   // The Bot Persona
-  "name"   : "Stuart",
+  "name"   : "tavuk",
   "emoji"  : ":construction_worker:",
 
   // The Slack host
   //
   // Careful! Don't include the token with your slack_host value
-  "slack_host"  : "https://FIXME.slack.com/services/hooks/incoming-webhook",
-  "slack_token" : "FIXME"
-
+  "slack_host"  : "https://hooks.slack.com/services/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",
+  "slack_token" : "JiR8JyEms3MOQ8uI6GfXorTB"
 };
